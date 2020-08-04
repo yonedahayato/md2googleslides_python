3a7a3f1d39872b87eac6fed524daf4677056cfae
yoneda <yoneda>
2020-08-04 17:18:19 +0900

    日本語ファイル名の問題が発生したので、改善

--- md2gslides.sh
+++ md2gslides.sh
@@ -6,5 +6,8 @@ echo markdown file name : ${MD_FILE_NAME}
 PPTX_FILE_NAME=${MD_FILE_NAME%.*}.pptx
 echo power point file name : ${PPTX_FILE_NAME}
 
-pandoc ${MD_FILE_NAME} -o ${PPTX_FILE_NAME}
+cp ${MD_FILE_NAME} tmp.md
+pandoc tmp.md -o tmp.pptx
+cp tmp.pptx ${PPTX_FILE_NAME}
+
 python3 ${SCRIPTPATH}/script/upload_to_drive.py ${PPTX_FILE_NAME}
