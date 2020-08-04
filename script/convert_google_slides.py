b1949846d65e8ff13976d623f9573099a9f192e9
yoneda <yoneda>
2020-08-04 14:04:51 +0900

    パワーポイントを作成するスクリプト

--- script/convert_google_slides.py
+++ script/convert_google_slides.py
@@ -1,54 +1,21 @@
-def test_convert_google_slides():
-    pass
+from pptx import Presentation
 
-# from __future__ import print_function
-import pickle
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
+def convert_slide(text):
+    """ convert_google_slides func
 
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/presentations.readonly']
+    convert to slide from text
 
-# The ID of a sample presentation.
-PRESENTATION_ID = '1EAYk18WDjIG-zp_0vLm3CsfQh_i8eXc67Jo2O9C6Vuc'
+    Args:
+        text(str): text want to convert
 
-def main():
-    """Shows basic usage of the Slides API.
-    Prints the number of slides and elments in a sample presentation.
     """
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+    prs = Presentation()
+    title_slide_layout = prs.slide_layouts[0]
+    slide = prs.slides.add_slide(title_slide_layout)
+    title = slide.shapes.title
+    subtitle = slide.placeholders[1]
 
-    service = build('slides', 'v1', credentials=creds)
+    title.text = "Hello, World!"
+    subtitle.text = "python-pptx was here!"
 
-    # Call the Slides API
-    presentation = service.presentations().get(
-        presentationId=PRESENTATION_ID).execute()
-    slides = presentation.get('slides')
-
-    print('The presentation contains {} slides:'.format(len(slides)))
-    for i, slide in enumerate(slides):
-        print('- Slide #{} contains {} elements.'.format(
-            i + 1, len(slide.get('pageElements'))))
-
-
-if __name__ == '__main__':
-    main()
+    prs.save('test.pptx')
