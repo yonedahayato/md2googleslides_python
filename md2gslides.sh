c4be0676b34550021f0ac718e6dc1be4c86ce3d0
yoneda <yoneda>
2020-08-04 16:06:16 +0900

    この shell file がどこからでも実行できるように修正

--- md2gslides.sh
+++ md2gslides.sh
@@ -1,3 +1,5 @@
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
 MD_FILE_NAME=$1
 echo markdown file name : ${MD_FILE_NAME}
 
@@ -5,4 +7,4 @@ PPTX_FILE_NAME=${MD_FILE_NAME%.*}.pptx
 echo power point file name : ${PPTX_FILE_NAME}
 
 pandoc ${MD_FILE_NAME} -o ${PPTX_FILE_NAME}
-python3 ./script/upload_to_drive.py ${PPTX_FILE_NAME}
+python3 ${SCRIPTPATH}/script/upload_to_drive.py ${PPTX_FILE_NAME}
