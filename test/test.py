814a100a24100c94c22dc72ee0d46f7e5b2f2d08
yoneda <yoneda>
2020-08-04 14:06:05 +0900

    test script 修正

--- test/test.py
+++ test/test.py
@@ -9,13 +9,20 @@ EXAMPLE_PATH = os.path.join(PARENTDIR,"examples", "example.md")
 sys.path.append(PARENTDIR)
 
 from script.read_markdown import read_markdown
+from script.convert_google_slides import convert_slide
+from script.upload_to_drive import upload_to_drive
 
 def test_read_markdown():
     read_markdown(EXAMPLE_PATH)
 
 def test_convert_google_slides():
-    text = read_markdown(EXAMPLE_PATH)
+    # text = read_markdown(EXAMPLE_PATH)
+    convert_slide("test")
+
+def test_upload_to_drive():
+    upload_to_drive("examples/example.md")
 
 if __name__ == "__main__":
     test_read_markdown()
     test_convert_google_slides()
+    test_upload_to_drive()
